daa606a0d957688e46b9f3c5456578a7dba615e9
Arthur Menezes <arthurmcorrea282@gmail.com>
2026-05-18 01:32:18 -0300

    finalizando tarefa, o servidor dispara para o android e o android salva na lista de conattos

--- server/server.cpp
+++ server/server.cpp
@@ -1,74 +1,56 @@
 #include <iostream>
-#include <hiredis/hiredis.h>
+#include <string>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h> // Para inet_addr
-#include <cstring>
+#include <arpa/inet.h>
 #include <unistd.h>
+#include <cstring>
 
-// Função para enviar o comando de salvar contato para o Android
-void enviarContatoParaAndroid(const char* androidIp, const std::string& nome, const std::string& telefone) {
+// Função para enviar o comando para o Android
+void disparar_contato(const std::string& ip_android, const std::string& nome, const std::string& fone) {
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        std::cerr << "Erro ao criar socket" << std::endl;
+        return;
+    }
+
     struct sockaddr_in servaddr;
-    
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(8081); // Porta que o Android está ouvindo
-    servaddr.sin_addr.s_addr = inet_addr(androidIp);
+    servaddr.sin_port = htons(8081); // Porta que o Android ouve
+    servaddr.sin_addr.s_addr = inet_addr(ip_android.c_str());
 
-    std::string mensagem = "SALVAR_CONTATO:" + nome + "," + telefone;
-    
+    std::string mensagem = "SALVAR_CONTATO:" + nome + "," + fone;
+
     sendto(sockfd, mensagem.c_str(), mensagem.length(), 0, 
            (const struct sockaddr *)&servaddr, sizeof(servaddr));
-    
-    std::cout << "Comando enviado para o Android: " << mensagem << std::endl;
+
+    std::cout << "Comando enviado para " << ip_android << ": " << mensagem << std::endl;
     close(sockfd);
 }
 
 int main() {
-    // 1. Conecta ao Redis
-    redisContext *c = redisConnect("127.0.0.1", 6379);
-    if (c == NULL || c->err) {
-        std::cerr << "Erro ao conectar no Redis!" << std::endl;
-        return 1;
-    }
-
-    // 2. Configura o Socket UDP para receber eventos do Android
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    struct sockaddr_in servaddr, cliaddr;
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(8080); // Porta que o servidor ouve o Android
-
-    bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr));
-
-    std::cout << "Servidor C++ aguardando eventos na porta 8080..." << std::endl;
-
-    char buffer[1024];
-    socklen_t len = sizeof(cliaddr);
+    std::string ip;
+    std::cout << "--- Disparador de Contatos C++ ---" << std::endl;
+    std::cout << "Digite o IP do celular Android: ";
+    std::cin >> ip;
 
     while (true) {
-        int n = recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr *)&cliaddr, &len);
-        buffer[n] = '\0';
-        
-        std::string info(buffer);
-        // Pega o IP do Android que enviou a mensagem
-        char *androidIp = inet_ntoa(cliaddr.sin_addr);
-        
-        std::cout << "Evento recebido de " << androidIp << ": " << info << std::endl;
+        std::string nome, telefone;
+        std::cout << "\n--- Novo Disparo ---" << std::endl;
+        std::cout << "Nome do contato: ";
+        std::cin.ignore(); // Limpar buffer
+        std::getline(std::cin, nome);
+        std::cout << "Telefone: ";
+        std::cin >> telefone;
 
-        // 3. Salva no Redis
-        redisReply *reply = (redisReply *)redisCommand(c, "SET ultimo_evento %s", info.c_str());
-        freeReplyObject(reply);
+        disparar_contato(ip, nome, telefone);
         
-        // 4. EXEMPLO: Ao receber o clique do botão, o servidor "responde" 
-        // mandando o Android salvar um contato de teste
-        std::cout << "Respondendo ao Android para salvar  um contato..." << std::endl;
-        enviarContatoParaAndroid(androidIp, "Intelitr Suporte", "11999999999");
+        std::cout << "Deseja enviar outro? (s/n): ";
+        char cont;
+        std::cin >> cont;
+        if (cont == 'n') break;
     }
 
-    redisFree(c);
-    close(sockfd);
     return 0;
 }
